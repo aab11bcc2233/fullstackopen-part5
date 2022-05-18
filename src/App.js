a6979b80f8972a2c3ddd60d9ff1ccfae0a74eb0a
hetianpeng <hetianpeng@dongyin.net>
2022-05-18 18:48:25 +0800

    part5 5.3

--- src/App.js
+++ src/App.js
@@ -6,9 +6,14 @@ import localUser from './utils/user'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
   const [user, setUser] = useState(null)
+
+  const [title, setTitle] = useState("")
+  const [author, setAuthor] = useState("")
+  const [url, setUrl] = useState("")
 
   const clickLogin = async (event) => {
     event.preventDefault()
@@ -16,6 +21,8 @@ const App = () => {
 
     try {
       const user = await loginService.login({ username, password })
+
+      blogService.setToken(user.token)
       localUser.save(user)
       setUser(user)
       setUsername('')
@@ -28,7 +35,28 @@ const App = () => {
   const clickLogout = (event) => {
     console.log(event)
     localUser.remove()
+    blogService.setToken(null)
     setUser(null)
+  }
+
+  const clickCreateBlog = async (event) => {
+    event.preventDefault()
+
+    try {
+      const data = await blogService.create({
+        title: title,
+        author: author,
+        url: url
+      })
+      console.log("create a new blog succeed", data)
+
+      setTitle("")
+      setAuthor("")
+      setUrl("")
+
+    } catch (error) {
+      console.log('create a new blog fails', error.response.data.error)
+    }
   }
 
 
@@ -36,6 +64,7 @@ const App = () => {
     const userFromLocal = localUser.get()
     if (userFromLocal) {
       setUser(userFromLocal)
+      blogService.setToken(userFromLocal.token)
     }
   }, [])
 
@@ -70,10 +99,30 @@ const App = () => {
         <strong>{user.name} logged in</strong> <button onClick={clickLogout}>logout</button>
       </div>
       <br />
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
+      <h2>create new</h2>
+      <div>
+        <form onSubmit={clickCreateBlog}>
+          <div>
+            title:<input type="text" value={title} onChange={({ target }) => setTitle(target.value)} />
+          </div>
+
+          <div>
+            author:<input type="text" value={author} onChange={({ target }) => setAuthor(target.value)} />
+          </div>
+
+          <div>
+            url:<input type="text" value={url} onChange={({ target }) => setUrl(target.value)} />
+          </div>
+          <button type="submit">create</button>
+        </form>
+      </div>
+
+      {
+        blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )
+      }
+    </div >
   )
 }
 
