b2b6746433d09bbeb401cb0bb18f306c286bec77
hetianpeng <hetianpeng@dongyin.net>
2022-05-20 14:38:12 +0800

    part5 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,33 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [viewAll, setViewAll] = useState(false)
+
+  const toggleViewAll = () => setViewAll(!viewAll)
+
+  const displayStyle = {
+    display: viewAll ? "" : "none"
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>{blog.title} <button onClick={toggleViewAll}>{viewAll ? "hide" : "view"}</button></div>
+
+      <div style={displayStyle}>
+        <div>{blog.url}</div>
+        <div>likes {blog.likes} <button>like</button></div>
+        <div>{blog.author}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
